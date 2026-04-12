5776852ee9df98d567338f63f75bb2e14f49cb38
202317b3242-hue <202317b3242@wilp.bits-pilani.ac.in>
2026-04-12 11:14:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # Indian-Real-Estate
 # what-if calculator for Indian Real Estate analyst
+import streamlit as st
+
+st.title("simple streamlit app")
+st.header("welcome")
+st.write("this is a simple streamlit app")
+
+name=st.text_input("enter your name")
+if name:
+    st.success(f"hello, {name}!  \nHow are you?")
+
+number= st.slider("pick your age", 0, 100)
+st.write(f"you age is {number}")
